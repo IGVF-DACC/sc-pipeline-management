9cc8bbe90b9aeaf3e779c4608669072cf6623e5c
Zhewei Shen <zwshen@stanford.edu>
2024-11-18 11:59:34 -0800

    moved igvf api to init

--- src/portal_to_terra_input_from_anaset.py
+++ src/portal_to_terra_input_from_anaset.py
@@ -1,7 +1,8 @@
 import urllib.parse
-from igvf_client import Configuration
-from igvf_client import ApiClient
-from igvf_client import IgvfApi
+# from igvf_client import Configuration
+# from igvf_client import ApiClient
+# from igvf_client import IgvfApi
+from src import get_igvf_auth_and_api
 import os
 import pandas as pd
 import itertools
@@ -10,20 +11,20 @@ from datetime import datetime
 
 # TODO: Add proper docstring
 
-def get_igvf_auth_and_api(igvf_site: str = 'production'):
-    """Set up IGVF data portal access and set up IGVF python client api.
-    """
-    if igvf_site == 'production':
-        host = 'https://api.data.igvf.org'
-    elif igvf_site == 'sandbox':
-        host = 'https://api.sandbox.igvf.org'
-    config = Configuration(
-        access_key=os.environ["IGVF_API_KEY"],
-        secret_access_key=os.environ["IGVF_SECRET_KEY"],
-        host=host
-    )
-    client = ApiClient(config)
-    return IgvfApi(client)
+# def get_igvf_auth_and_api(igvf_site: str = 'production'):
+#     """Set up IGVF data portal access and set up IGVF python client api.
+#     """
+#     if igvf_site == 'production':
+#         host = 'https://api.data.igvf.org'
+#     elif igvf_site == 'sandbox':
+#         host = 'https://api.sandbox.igvf.org'
+#     config = Configuration(
+#         access_key=os.environ["IGVF_API_KEY"],
+#         secret_access_key=os.environ["IGVF_SECRET_KEY"],
+#         host=host
+#     )
+#     client = ApiClient(config)
+#     return IgvfApi(client)
 
 
 #  HACK: A map that translates an assay term to either ATAC or RNA
