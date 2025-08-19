eb8e1712e477e5f07440e42be0dfaa6809e8f6c5
Zhewei Shen <zwshen@stanford.edu>
2025-08-19 11:02:50 -0700

    WIP:seqspec parsing

---
+++ src/sc_pipe_management/input_params_generation/seqspec_parsing.py
@@ -0,0 +1,16 @@
+import urllib.parse
+import os
+import pandas as pd
+import itertools
+import urllib
+from datetime import datetime
+import subprocess
+import requests
+from collections import OrderedDict
+from itertools import chain
+from seqspec.utils import load_spec
+import seqspec
+import re
+import logging
+
+import sc_pipe_management.input_params_generation.constant as const
